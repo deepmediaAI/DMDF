6a986295c4e881ea882060c055fa87b9f899788b
OfmanAI <105573972+OfmanAI@users.noreply.github.com>
2023-04-28 11:53:07 -0600

    Update DMDF_V2_Extraction.py

--- DMDF_V2_Extraction.py
+++ DMDF_V2_Extraction.py
@@ -15,3 +15,16 @@ for input_zip in input_zips:
     unzip_directory = os.path.dirname(input_zip)
     shutil.unpack_archive(input_zip, unzip_directory)
     os.remove(input_zip)
+    
+#For Aligned Image Extraction
+
+aligned_input_zips = []
+aligned_input_zips += sorted(Path(dataset_path).glob('**/*/*.zip'))
+aligned_input_zips = list(set(input_zips))
+aligned_input_zips = [str(path) for path in input_zips]
+aligned_input_zips = sorted(input_zips)
+
+for input_zip in aligned_input_zips:
+    unzip_directory = os.path.dirname(input_zip)
+    shutil.unpack_archive(input_zip, unzip_directory)
+    os.remove(input_zip)
